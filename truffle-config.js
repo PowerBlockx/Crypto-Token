8aa5bc5e822a50b0d6787e0c73c9db5285890b09
0Verlord-41 <kinicooo1@gmail.com>
2021-11-30 12:50:02 +0530

    Update truffle-config.js
    Added configuration for Rinkeby Test network

--- truffle-config.js
+++ truffle-config.js
@@ -40,6 +40,12 @@ module.exports = {
       host: "127.0.0.1",
       port: "7545",
       network_id: "*"
+    },
+    rinkeby: {
+      host: "localhost",
+      port: 8545,
+      network_id: 4,
+      gas: 4700000
     }
     // Useful for testing. The `development` name is special - truffle uses it by default
     // if it's defined here and no other network is specified at the command line.
